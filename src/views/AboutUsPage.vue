9a9d80d5899c9c0ec17625bc657ed6dcfb6fb3e0
david <dsolanoreyes@my.wctc.edu>
2022-09-11 14:04:45 -0500

    ʕ•́ᴥ•̀ʔっ

--- src/views/AboutUsPage.vue
+++ src/views/AboutUsPage.vue
@@ -1,12 +1,93 @@
 <template>
 <div>
+  <v-row class="ma-0">
+    <v-col cols sm="12" lg="6">
+      <v-parallax src="https://tuiopay.com/wp-content/uploads/2018/12/OJXK980-846x846.jpg"></v-parallax>
+    </v-col>
 
+    <v-col cols sm="12" lg="6">
+      <v-card elevation="0">
+        <v-card-title>
+          Teaching With Purpose
+        </v-card-title>
+
+        <v-card-subtitle>
+          <strong>If teachers teach us who teaches teachers?</strong>
+        </v-card-subtitle>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborumvo luptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col cols lg="3"></v-col>
+
+    <v-col cols sm="12" lg="6">
+      <v-img src="https://i.pinimg.com/736x/f9/6d/42/f96d42573ac8dc7ee16906c045d2ffa7.jpg"></v-img>
+    </v-col>
+
+    <v-col cols lg="3"></v-col>
+  </v-row>
+
+  <v-row>
+    <v-col>
+      <v-card>
+        <v-card-title>Training</v-card-title>
+        <v-card-subtitle><strong>Being a student again</strong></v-card-subtitle>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborumvo luptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col>
+      <v-card>
+        <v-card-title>Specialty</v-card-title>
+        <v-card-subtitle><strong>Helping you grow</strong></v-card-subtitle>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborumvo luptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </div>
 </template>
 
 <script>
 export default {
-  name: "AboutUsPage"
+  name: "AboutUsPage",
 }
 </script>
 
